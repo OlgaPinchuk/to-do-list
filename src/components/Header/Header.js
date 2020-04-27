7dc75769bd45923b2f9a6b660215948b63091c9d
olga pinchuk <pinchukolya@gmail.com>
2020-04-27 17:17:30 +0200

    add(Header): add Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "./Header.css";
+
+export default ({ onFormSubmit }) => {
+  const [todoValue, setTodoValue] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onFormSubmit(todoValue);
+    setTodoValue("");
+  };
+
+  return (
+    <header className="header">
+      <h1>Your tasks</h1>
+      <form className="task-input-container" onSubmit={handleSubmit}>
+        <input
+          className="task-input"
+          type="text"
+          placeholder="Add new task"
+          onChange={(e) => setTodoValue(e.target.value)}
+          value={todoValue}
+        />
+        <button className="add-btn">Add task</button>
+      </form>
+    </header>
+  );
+};
