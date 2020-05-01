f36d0f8c4dadbe12935d3546099d05791762b01a
olga pinchuk <pinchukolya@gmail.com>
2020-05-01 20:48:56 +0200

    update(ToDoList): add  completed functionality

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -1,24 +1,39 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./ToDoList.css";
+import Input from "../common/Input";
 
-const ToDoList = ({ todos }) => {
+const ToDoList = ({ activeTodos, completeTodo }) => {
   return (
     <ol className="task-list">
-      {todos.map((item) => (
-        <li key={item.id}>{item.text}</li>
+      {activeTodos.map((item) => (
+        <div
+          className="list-item"
+          key={item.id}
+          onClick={() => completeTodo(item.id)}
+        >
+          <Input
+            type="checkbox"
+            name="select-done"
+            id={item.id}
+            className="done-checkbox"
+          />
+          <label
+            htmlFor={item.id}
+            className="done-checkbox-label"
+            data-content={item.text}
+          >
+            <li>{item.text}</li>
+          </label>
+        </div>
       ))}
     </ol>
   );
 };
 
 ToDoList.propTypes = {
-  todos: PropTypes.arrayOf(
-    PropTypes.shape({
-      id: PropTypes.number.isRequired,
-      text: PropTypes.string.isRequired,
-    })
-  ),
+  completeTodo: PropTypes.func.isRequired,
+  activeTodos: PropTypes.array.isRequired,
 };
 
 export default ToDoList;
