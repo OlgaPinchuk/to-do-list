5226b9f22335602304b8a3b14a7e356f42bfbc81
olga pinchuk <pinchukolya@gmail.com>
2020-05-01 20:49:33 +0200

    update(App): add active todos

--- src/App.js
+++ src/App.js
@@ -5,15 +5,23 @@ import ToDoList from "./components/ToDoList/ToDoList";
 
 function App() {
   const [todos, setTodos] = useState([]);
+  let activeTodos = todos.filter((item) => !item.complete);
 
   const saveTodo = (todoItem) => {
     setTodos([...todos, todoItem]);
   };
 
+  const completeTodo = (todoId) => {
+    const newTodos = [...todos];
+    const newTodoIndex = newTodos.findIndex((item) => item.id === todoId);
+    newTodos[newTodoIndex].complete = true;
+    setTodos([...newTodos]);
+  };
+
   return (
     <>
-      <Header onFormSubmit={saveTodo} />
-      <ToDoList todos={todos} />
+      <Header saveTodo={saveTodo} />
+      <ToDoList activeTodos={activeTodos} completeTodo={completeTodo} />
     </>
   );
 }
