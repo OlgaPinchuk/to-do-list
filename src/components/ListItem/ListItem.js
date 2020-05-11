329f165db08e5db123b6b891368267227263f551
olga pinchuk <pinchukolya@gmail.com>
2020-05-11 12:33:19 +0200

    refactor(ListItem): refactoring

--- src/components/ListItem/ListItem.js
+++ src/components/ListItem/ListItem.js
@@ -2,24 +2,22 @@ import React from "react";
 import PropTypes from "prop-types";
 import Checkbox from "../common/Checkbox/Checkbox";
 
-const ListItem = ({ id, complete, text, completeTodo }) => {
-  return (
-    <div className="list-item" onClick={() => completeTodo(id)}>
-      <Checkbox
-        name="select-done"
-        id={id}
-        className="done-checkbox"
-        disabled={complete}
-      />
-      <label htmlFor={id} className="done-checkbox-label" data-content={text}>
-        {text}
-      </label>
-    </div>
-  );
-};
+const ListItem = ({ id, complete, text, completeTodo }) => (
+  <div className="list-item" onClick={() => completeTodo(id)}>
+    <Checkbox
+      name="select-done"
+      id={id}
+      className="done-checkbox"
+      disabled={complete}
+    />
+    <label htmlFor={id} className="done-checkbox-label" data-content={text}>
+      {text}
+    </label>
+  </div>
+);
 
 ListItem.propTypes = {
-  id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  id: PropTypes.number.isRequired,
   complete: PropTypes.bool.isRequired,
   text: PropTypes.string.isRequired,
   completeTodo: PropTypes.func.isRequired,
