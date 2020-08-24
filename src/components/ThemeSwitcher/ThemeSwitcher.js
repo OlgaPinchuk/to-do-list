4e060f2f13c669b24d21eb29605d2bcc86c5c66b
olga pinchuk <pinchukolya@gmail.com>
2020-08-24 20:02:24 +0200

    add ThemeSwitcher component

---
+++ src/components/ThemeSwitcher/ThemeSwitcher.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "./ThemeSwitcher.css";
+
+import { useDispatch, useSelector } from "react-redux";
+import { toggleTheme } from "../../behavior/theme";
+
+const ThemeSwitcher = () => {
+  const theme = useSelector(({ theme }) => theme);
+  const themeToSwitch = theme === "dark" ? "light" : "dark";
+  const dispatch = useDispatch();
+
+  return (
+    <button
+      onClick={() => dispatch(toggleTheme())}
+      className="theme-switch-btn"
+    >
+      Switch theme to {themeToSwitch}
+    </button>
+  );
+};
+
+export default ThemeSwitcher;
